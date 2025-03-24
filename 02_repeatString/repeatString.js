0ec27aaf9a8d2af3f980cbf33a6f91dc2f4c1a40
Kuzrr <ptrk7331@gmail.com>
2025-03-24 20:40:50 +0100

    done ex 01 - 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeat) {
+    let result = ''
+    if (repeat < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < repeat ; i++){
+        result = result + text;
+    }
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverse = '';
+    for(let i = 1; i<= text.length; i++){
+        let currentLastLetter = text.charAt(text.length - i);
+        reverse = reverse + currentLastLetter;
+    }
 
+    return reverse;
 };
+
+console.log(reverseString("Hello World"))
 
 // Do not edit below this line
 module.exports = reverseString;
+``
