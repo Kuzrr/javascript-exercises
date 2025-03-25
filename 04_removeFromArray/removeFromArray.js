bd4a088d8fa59bc7413cee551ecda8d0f28efcb4
Kuzrr <ptrk7331@gmail.com>
2025-03-25 20:35:59 +0100

    do 04 and 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i <= arguments.length - 1; i++) {
+        let toRemove = arguments[i];
+        let indexToRemove = array.indexOf(toRemove);
+        while(indexToRemove !== -1){
+            array.splice(indexToRemove, 1);
+            indexToRemove = array.indexOf(toRemove);
+        }
+    }
+    return array;
 };
+
+ console.log(removeFromArray([5, 1, 2, 5, 3, 11], 5));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)  || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    let min = 0;
+    let max = 0;
+    if(num1 < num2){
+         min = num1;
+         max = num2;
+    }else{
+         min = num2;
+         max = num1
+    }
+    sum = 0;
+    while (min <= max) {
+        sum += min;
+        min++;
+    }
+    return sum;
 };
+
+console.log(sumAll(3,1));
 
 // Do not edit below this line
 module.exports = sumAll;
